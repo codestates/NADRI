713931fb619816107f51c993fb7fd5be29615f5a
LeeTaeGwan <freshman1998@naver.com>
2022-01-01 17:31:32 +0900

    헤더 프레임 구성

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
+
+const FooterContainer = styled.footer`
+  width: 100vw;
+  height: 20vh;
+  background-color: #343a40;
+`
+
+export default function Footer () {
+  return (
+    <FooterContainer>
+    </FooterContainer>
+  )
+}
