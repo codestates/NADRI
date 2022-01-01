e98840f569f390faf932775c936b5a261ad5b66f
LeeTaeGwan <freshman1998@naver.com>
2022-01-01 23:12:39 +0900

    Footer 구현

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import MainPage from '../Pages/MainPage';
 import { Routes, Route, useNavigate, Navigate, BrowserRouter, Link } from 'react-router-dom';
 
 export default function Body () {
